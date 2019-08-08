eaa38e614e9a2c9b27b1c5d3037faafe62737989
liangtaoray1 <liangtao@sohero.com>
2019-08-08 14:44:22 +0800

    登陆

--- common/src/main/java/com/jeesite/common/constant/ServiceConstant.java
+++ common/src/main/java/com/jeesite/common/constant/ServiceConstant.java
@@ -5,5 +5,7 @@ package com.jeesite.common.constant;
  */
 public class ServiceConstant {
 
+    //登陆功能
+    public static final String TEACHER_SIDE_LOGIN ="/common/commonUser/teacherSideLogin";
 
 }
